cb44033ddcd68e940e12245de70cff1d379a326d
Bagus_24 <Bagus_24@Bagus24>
2020-11-18 21:11:06 +0700

    new commit

---
+++ src/model/Koordinat.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Bagus_24
+ * NAMA         : Bagus Budi Wibowo
+ * KELAS        : PBOIF2 
+ * NIM          : 10119064 
+ * Deskripsi Program : Program ini merupakan program untuk menampilkan 
+ * koordinat warna
+ */
+public class Koordinat {
+    protected int x;
+    protected int y;
+
+    public Koordinat(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+    
+    
+}
---
+++ src/pboif2/pkg10119064/latihan54/koordinat/PBOIF210119064Latihan54Koordinat.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119064.latihan54.koordinat;
+
+import model.Koordinat;
+import model.WarnaKoordinat;
+
+/**
+ *
+ * @author Bagus_24
+ * NAMA         : Bagus Budi Wibowo
+ * KELAS        : PBOIF2 
+ * NIM          : 10119064 
+ * Deskripsi Program : Program ini merupakan program untuk menampilkan 
+ * koordinat warna
+ */
+public class PBOIF210119064Latihan54Koordinat {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        WarnaKoordinat warnaKoordinat = new WarnaKoordinat(10, 4, "Jingga");
+                
+        System.out.println("Warna Koordinat : " + warnaKoordinat.getNamaWarna());
+        System.out.println("Koordinat Sumbu x : " + warnaKoordinat.getX() + ", y : " + warnaKoordinat.getY());
+    }
+    
+}
